362e2fda665274b22b0b5fa35349f12fc6de1a32
Inon Bercu <inonbr@gmail.com>
2021-04-19 13:18:03 +0300

    login router is done

--- back_end/routers/users.js
+++ back_end/routers/users.js
@@ -66,4 +66,53 @@ router.post(
   }
 );
 
+router.post(
+  '/login',
+  [
+    check('email', 'Please includ a valid email').trim().isEmail(),
+    check('password', 'Please enter a valid password').trim().not().isEmpty(),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+
+      // if there are errors return 400 (bed request);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      const { email, password } = req.body;
+
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(400).json({ msg: 'invalid credentials' });
+      }
+
+      const compare = await bcrypt.compare(password, user.password);
+
+      if (!compare) {
+        return res.status(400).json({ msg: 'invalid credentials' });
+      }
+
+      const token = jwt.sign(
+        {
+          id: user._id,
+          username: user.username,
+          email: user.email,
+        },
+        process.env.TOKEN
+      );
+
+      return res.status(200).json({
+        msg: 'user logged in successfully',
+        token,
+      });
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send({ err: 'Server error', message: err.message });
+    }
+  }
+);
+
 module.exports = router;
