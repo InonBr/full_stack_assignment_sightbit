f7de5d077b181683057779a374bcb2284391e36a
Inon Bercu <inonbr@gmail.com>
2021-04-26 09:38:47 +0300

    react app using redux setup is done

---
+++ react_redux_front_end/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>hello world</h1>
+      <Button variant='primary'>Primary</Button>
+    </div>
+  );
+}
+
+export default App;
